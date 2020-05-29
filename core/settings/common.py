d5b037d42f22bb4fd22d503e35dca2fc486e9e95
TralahM <musyoki.brian@tralahtek.com>
2020-05-29 22:02:19 +0300

    whitenoise middleware and staticfile storage default Signed-off-by: TralahM <musyoki.brian@tralahtek.com>

--- core/settings/common.py
+++ core/settings/common.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -114,3 +115,4 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "core.storage.WhiteNoiseStaticFilesStorage"
